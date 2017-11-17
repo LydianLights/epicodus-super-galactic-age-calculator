3fddc8c2603ad77a014dbe8043d0771cae669a5d
LydianLights <rane.fields@gmail.com>
2017-11-17 10:00:05 -0800

    Add planetary date conversions

--- js/Age.js
+++ js/Age.js
@@ -2,4 +2,22 @@ export class Age {
   constructor(birthday) {
     this.origin = birthday;
   }
+  getInSeconds() {
+    return Math.floor((new Date().getTime() - this.origin.getTime()) / 1000);
+  }
+  getInYears() {
+    return this.getInSeconds() / (60 * 60 * 24 * 365);
+  }
+  getInMercuryYears() {
+    return this.getInYears() * .24;
+  }
+  getInVenusYears() {
+    return this.getInYears() * .62;
+  }
+  getInMarsYears() {
+    return this.getInYears() * 1.88;
+  }
+  getInJupiterYears() {
+    return this.getInYears() * 11.86;
+  }
 }
--- spec/Age-spec.js
+++ spec/Age-spec.js
@@ -7,3 +7,51 @@ describe("Age constructor", function() {
     expect(testAge.origin).toEqual(birthday);
   });
 });
+
+describe("Age getInSeconds", function() {
+  it("should give a person's age in seconds" , function() {
+    let birthday = new Date(1991, 5, 3);
+    let testAge = new Age(birthday);
+    expect(testAge.getInSeconds()).not.toBeLessThan(835007306);
+  });
+});
+
+describe("Age getInYears", function() {
+  it("should give a person's age in years" , function() {
+    let birthday = new Date(1991, 5, 3);
+    let testAge = new Age(birthday);
+    expect(testAge.getInYears()).not.toBeLessThan(26);
+  });
+});
+
+describe("Age getInMercuryYears", function() {
+  it("should give a person's age in years" , function() {
+    let birthday = new Date(1991, 5, 3);
+    let testAge = new Age(birthday);
+    expect(testAge.getInMercuryYears()).not.toBeLessThan(26 * .24);
+  });
+});
+
+describe("Age getInVenusYears", function() {
+  it("should give a person's age in years" , function() {
+    let birthday = new Date(1991, 5, 3);
+    let testAge = new Age(birthday);
+    expect(testAge.getInVenusYears()).not.toBeLessThan(26 * .62);
+  });
+});
+
+describe("Age getInMarsYears", function() {
+  it("should give a person's age in years" , function() {
+    let birthday = new Date(1991, 5, 3);
+    let testAge = new Age(birthday);
+    expect(testAge.getInMarsYears()).not.toBeLessThan(26 * 1.88);
+  });
+});
+
+describe("Age getInJupiterYears", function() {
+  it("should give a person's age in years" , function() {
+    let birthday = new Date(1991, 5, 3);
+    let testAge = new Age(birthday);
+    expect(testAge.getInJupiterYears()).not.toBeLessThan(26 * 11.86);
+  });
+});
