048acb451b971bd668d9578765d7434519496dbb
LydianLights <rane.fields@gmail.com>
2017-11-17 09:00:25 -0800

    Add age class constructor and tests

---
+++ js/Age.js
@@ -0,0 +1,5 @@
+export class Age {
+  constructor(birthday) {
+    this.origin = birthday;
+  }
+}
--- spec/Age-spec.js
+++ spec/Age-spec.js
@@ -1,5 +1,9 @@
-describe('Age', function() {
-  it('should...', function() {
-    // Test here
+import { Age } from "./../js/Age.js"
+
+describe("Age constructor", function() {
+  it("should find a person's age based on their birthday" , function() {
+    let birthday = new Date(1991, 5, 3);
+    let testAge = new Age(birthday);
+    expect(testAge.origin).toEqual(birthday);
   });
 });
