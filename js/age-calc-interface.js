9d45393964ae0b0ea0c9ce6249b292d174b299ec
LydianLights <rane.fields@gmail.com>
2017-11-17 16:04:20 -0800

    Add pagination to form

---
+++ js/age-calc-interface.js
@@ -0,0 +1,51 @@
+class UserInfoForm {
+  constructor() {
+    this.lastPage = 5;
+    this.currentPage = 1;
+  }
+  goToPage(page) {
+    const animationSpeed = 200;
+    if (page >= 1 && page <= this.lastPage) {
+      this.currentPage = page;
+      for (let i = 1; i <= this.lastPage; i++) {
+        $("#user-info-page-" + i).slideUp(animationSpeed);
+      }
+      $("#user-info-page-" + this.currentPage).slideDown(animationSpeed);
+      if (this.currentPage === 1) {
+        $("#user-info-form button[name=previous-button]").hide();
+      }
+      else {
+        $("#user-info-form button[name=previous-button]").show();
+      }
+      if (this.currentPage === this.lastPage) {
+        $("#user-info-form button[name=next-button]").hide();
+        $("#user-info-form button[name=submit-button]").show();
+      }
+      else {
+        $("#user-info-form button[name=next-button]").show();
+        $("#user-info-form button[name=submit-button]").hide();
+      }
+      console.log(this.currentPage);
+    }
+  }
+  goToNextPage() {
+    if (this.currentPage < this.lastPage) {
+      this.goToPage(this.currentPage + 1);
+    }
+  }
+  goToPreviousPage() {
+    if (this.currentPage > 1) {
+      this.goToPage(this.currentPage - 1);
+    }
+  }
+}
+
+$(document).ready(function() {
+  let userInfoForm = new UserInfoForm();
+  $("#user-info-form button[name=next-button]").click(function() {
+    userInfoForm.goToNextPage();
+  });
+  $("#user-info-form button[name=previous-button]").click(function() {
+    userInfoForm.goToPreviousPage();
+  });
+});
