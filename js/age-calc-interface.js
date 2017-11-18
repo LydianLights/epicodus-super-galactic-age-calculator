dbec44995c741b6602207b6e9e89d649236e6f80
LydianLights <rane.fields@gmail.com>
2017-11-17 17:04:44 -0800

    Gather form results into results class

--- js/age-calc-interface.js
+++ js/age-calc-interface.js
@@ -1,7 +1,56 @@
+import { HumanSubject } from "./../js/HumanSubject.js"
+import { Age } from "./../js/Age.js"
+
+class Page {
+  constructor() {
+    this.id = "6FE9BA";
+    this.userInfoForm = new UserInfoForm();
+    this.results = null;
+    this.initialize();
+  }
+  initialize() {
+    $("#user-info-form form").submit(function() {
+      event.preventDefault();
+      this.userInfoForm.goToNextPage();
+      if (this.userInfoForm.isSubmitted) {
+        this.getResultsFromForm();
+        this.showResults();
+      }
+    }.bind(this));
+    $("#user-info-form button[name=previous-button]").click(function() {
+      this.userInfoForm.goToPreviousPage();
+    }.bind(this));
+  }
+  getResultsFromForm() {
+    let id = this.id;
+    let localizedName = $("#user-info-first-name").val() + " " + $("#user-info-last-name").val();
+    let birthdateArray = $("#user-info-birthdate").val().split("-");
+    let birthdate = new Date(birthdateArray[0], birthdateArray[1], birthdateArray[2]);
+    let healthFactors = {
+      hereditaryHealthRating: parseFloat($("input:radio[name=user-info-hereditary-health]:checked").val()),
+      adequateExerciseRating: parseFloat($("input:radio[name=user-info-exercise]:checked").val()),
+      adequateHealthcareRating: parseFloat($("input:radio[name=user-info-healthcare]:checked").val()),
+      isSmoker: ($("#user-info-smoker").val() === "true"),
+      alcoholDependencyFactor: parseFloat($("#user-info-alcohol-dependency").val()),
+      radiationExposureFactor: parseFloat($("#user-info-radiation-exposure").val()),
+      immuneSystemCyberneticsInstalled: ($("#user-info-immune-system-cybernetics").val() === "true"),
+      uploadedToUniversalCollectiveConsciousness: ($("#user-info-universal-collective").val() === "true"),
+      missingSemicolonStressFactor: parseFloat($("#user-info-semicolon-stress").val()),
+      exposureToKarmaUnitTestingTool: ($("#user-info-karma").val() === "true")
+    };
+    let subject = new HumanSubject(id, localizedName, birthdate, healthFactors);
+    this.results = new Results(subject);
+  }
+  showResults() {
+    console.log(this.results);
+  }
+}
+
 class UserInfoForm {
   constructor() {
     this.lastPage = 5;
     this.currentPage = 1;
+    this.isSubmitted = false;
   }
   goToPage(page) {
     const animationSpeed = 200;
@@ -11,25 +60,14 @@ class UserInfoForm {
         $("#user-info-page-" + i).slideUp(animationSpeed);
       }
       $("#user-info-page-" + this.currentPage).slideDown(animationSpeed);
-      if (this.currentPage === 1) {
-        $("#user-info-form button[name=previous-button]").hide();
-      }
-      else {
-        $("#user-info-form button[name=previous-button]").show();
-      }
-      if (this.currentPage === this.lastPage) {
-        $("#user-info-form button[name=next-button]").hide();
-        $("#user-info-form button[name=submit-button]").show();
-      }
-      else {
-        $("#user-info-form button[name=next-button]").show();
-        $("#user-info-form button[name=submit-button]").hide();
-      }
     }
   }
   goToNextPage() {
     if (this.currentPage < this.lastPage) {
       this.goToPage(this.currentPage + 1);
+    }
+    else if (this.currentPage === this.lastPage) {
+      this.submitForm();
     }
   }
   goToPreviousPage() {
@@ -37,15 +75,17 @@ class UserInfoForm {
       this.goToPage(this.currentPage - 1);
     }
   }
+  submitForm() {
+    this.isSubmitted = true;
+  }
+}
+
+class Results {
+  constructor(humanSubject) {
+    this.humanSubject = humanSubject;
+  }
 }
 
 $(document).ready(function() {
-  let userInfoForm = new UserInfoForm();
-  $("#user-info-form form").submit(function() {
-    event.preventDefault();
-    userInfoForm.goToNextPage();
-  });
-  $("#user-info-form button[name=previous-button]").click(function() {
-    userInfoForm.goToPreviousPage();
-  });
+  let page = new Page();
 });
